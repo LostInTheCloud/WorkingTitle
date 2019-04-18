ea61af716df79f2beea2344d0c71933bdadea173
Unknown <s6mahapp@uni-bonn.de>
2019-04-18 10:59:40 +0200

    GIT COMMIT!

--- source/main.h
+++ source/main.h
@@ -1,3 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <inttypes.h>
+
+struct registers 
+{
+    uint8_t     A,
+                F,  // flags TODO: get single flag bits
+                B,
+                C,
+                D,
+                E,
+                H,
+                L;
+    uint16_t    SP, // stack pointer
+                PC; // program counter
+}
+
+typedef struct registers registers;
