f298a31e31afe9313bfa3e72ea0d58e315de6df5
Lenni Hein <computerscience@uni-bonn.de>
2019-04-25 11:16:13 +0200

    qwe

--- source/main.c
+++ source/main.c
@@ -1,7 +1,25 @@
 #include "main.h"
 
+// !!!!!!!!!!!!!!!!!!!!!!
+// WHAT ABOUT NOPS vs 4 cycle instructions????
+// !!!!!!!!!!!!!!!!!!!!!!
+
 int main()
 {
     printf("Hello, DMG\n");
+
+    loop:
+    // gettime();
+
+    // do CPU Instruction
+    printf("x\n");
+
+    // gettime();
+
+    // get time difference
+
+    // sleep(250ns - t2 + t1 = 250 - delta t);
+    goto loop;
+    
     return EXIT_SUCCESS;
 }
--- source/main.h
+++ source/main.h
@@ -2,18 +2,34 @@
 #include <stdlib.h>
 #include <inttypes.h>
 
-struct registers 
-{
-    uint8_t     A,
-                F,  // flags TODO: get single flag bits
-                B,
-                C,
-                D,
-                E,
-                H,
-                L;
-    uint16_t    SP, // stack pointer
-                PC; // program counter
-}
+void* reg_ptr[12];
 
-typedef struct registers registers;
+#define F *((uint8_t*) reg_ptr + 0)
+#define A *((uint8_t*) reg_ptr + 1)
+#define C *((uint8_t*) reg_ptr + 2)
+#define B *((uint8_t*) reg_ptr + 3)
+#define E *((uint8_t*) reg_ptr + 4)
+#define D *((uint8_t*) reg_ptr + 5)
+#define L *((uint8_t*) reg_ptr + 6)
+#define H *((uint8_t*) reg_ptr + 7)
+
+#define SP *((uint16_t*) reg_ptr + 8/2)
+#define PC *((uint16_t*) reg_ptr + 10/2)
+
+#define BC *((uint16_t*) reg_ptr + 2/1)
+#define DE *((uint16_t*) reg_ptr + 4/2)
+#define HL *((uint16_t*) reg_ptr + 6/2)
+
+void print_regs(void)
+{
+    // 18 characters per line
+
+    printf("##################\n");
+    printf("A: 0x%2"PRIx8"    F: 0x%2"PRIx8"\n", A, F);
+    printf("B: 0x%2"PRIx8"    C: 0x%2"PRIx8"\n", B, C);
+    printf("D: 0x%2"PRIx8"    E: 0x%2"PRIx8"\n", D, E);
+    printf("H: 0x%2"PRIx8"    L: 0x%2"PRIx8"\n", H, L);
+    printf("     SP: 0x%2"PRIx16"\n", SP);
+    printf("     PC: 0x%2"PRIx16"\n", PC);
+    printf("##################\n");
+}
